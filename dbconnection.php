1f8de52969845e8ccf5668abc313ef56682d9231
BPchenlei <cheniseleigh.caro.cics@ust.edu.ph>
2025-05-28 01:21:39 +0800

    Create dbconnection.php

---
+++ dbconnection.php
@@ -0,0 +1,17 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbase = "db_ordering_system";
+
+$conn = new mysqli($servername,$username,$password,$dbase);
+
+// if ($conn->connect_error) {
+//     die("Connection error");
+// }else {
+//     echo "Connected";
+// }
+
+
+?>
